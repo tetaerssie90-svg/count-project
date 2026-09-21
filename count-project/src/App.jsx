59500689c99059e431f8497dd420d46859914bb6
tetaerssie90-svg <tetaerssie90@gmail.com>
2026-09-21 11:25:17 +0200

    creating a counter to increase and decrease

---
+++ count-project/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+export default function App (){
+  const [count, setCount] = React.useState(0)
+
+  function add (){
+    setCount(count + 1)
+  }
+
+  function sub (){
+    setCount (count - 1)
+  }
+
+  return (
+    <main className= "counter">
+      <h1>How many times will Bob say "state" in this section ?</h1>
+
+      <div className="count">
+        <button classsName="minus" onClick={sub} aria-label="Decrease count">-</button>
+
+        <h2 className="count">{count}</h2>
+
+        <button className="plus" onClick={add} aria-label="Increase count">+</button>
+
+
+
+      </div>
+
+
+    </main>
+  )
+}
